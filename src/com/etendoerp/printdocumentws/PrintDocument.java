2db6c2f2440411f13514aacddc8bdca058f6043e
Matias Bernal <matias.bernal@etendo.software>
2025-04-18 16:32:39 -0300

    Feature ETP-1461: Update module to support Etendo 25Q1

--- src/com/etendoerp/printdocumentws/PrintDocument.java
+++ src/com/etendoerp/printdocumentws/PrintDocument.java
@@ -7,7 +7,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.openbravo.base.ConfigParameters;
 import org.openbravo.base.HttpBaseUtils;
 import org.openbravo.base.exception.OBException;

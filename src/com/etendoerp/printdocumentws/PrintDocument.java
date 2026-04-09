f90bc98e2c0767376aed62baec47f2b80a15b18e
Román Magnoli <roman.magnoli@smfconsulting.es>
2026-04-09 17:07:00 -0300

    Feature ETP-3674: Hibernate 6 and Jakarta EE migration core fixes

--- src/com/etendoerp/printdocumentws/PrintDocument.java
+++ src/com/etendoerp/printdocumentws/PrintDocument.java
@@ -3,9 +3,9 @@ package com.etendoerp.printdocumentws;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
 import org.openbravo.base.ConfigParameters;
